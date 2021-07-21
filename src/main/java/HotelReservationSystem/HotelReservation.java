0be8037c87c9fa4ceb89c4432615b8861ae2df26
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-21 09:16:13 -0700

    Initial Commit: Welcome To Hotel Reservation Program

---
+++ src/main/java/HotelReservationSystem/HotelReservation.java
@@ -0,0 +1,11 @@
+package HotelReservationSystem;
+
+public class HotelReservation
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome To Hotel Reservation Program");
+		
+	}
+
+}
