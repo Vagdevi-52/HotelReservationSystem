284530af79eadd7a03c0b6ae89d83ceeb916ff4f
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-22 09:37:48 -0700

    Added: Program To Find The Cheapest Best Rated Hotel For A Given Date Range

--- src/test/java/HotelReservationSystem/HotelReservationTest.java
+++ src/test/java/HotelReservationSystem/HotelReservationTest.java
@@ -1,5 +1,6 @@
 package HotelReservationSystem;
 
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -76,7 +77,7 @@ public class HotelReservationTest
 	}
 	
 	@Test
-	public void whenHotelAddedToSystemRatingShouldGetAdded() // rating to the hotel.
+	public void whenHotelAddedToSystemRatingShouldGetAdded() 
 	{
 		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
 		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
@@ -89,5 +90,21 @@ public class HotelReservationTest
 		boolean result = hotelList.get(0).getRating() == 3 && hotelList.get(1).getRating() == 4
 				&& hotelList.get(2).getRating() == 5;
 		assertTrue(result);
+	}
+	
+	@Test
+	public void whenGivenDateRangeShouldReturnCheapestBestRatedHotels() // cheapest best rated hotels.
+	{
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 110, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		Map<Hotel, Integer> result = hotelReservation.getCheapestAndBestRatedHotels("11Sep2020", "12Sep2020");
+		result.forEach(
+				(k, v) -> System.out.println(k.getName() + ", Rating : " + k.getRating() + " and Total Rate " + v));
+		assertNotNull(result);
 	}
 }
